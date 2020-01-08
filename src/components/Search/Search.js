33c98874efb5d172480741ff47a52e35d458ce1e
Alan G Sinimbú <ar.redael@gmail.com>
2020-01-08 16:59:00 -0300

    styling Search component

---
+++ src/components/Search/Search.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Search.css'
+
+function Search() {
+  return (
+    <div>
+      <div className="form-group flex-wrap: wrap">
+        <label>Filter by name</label>
+        <div class="form-group has-search">
+          <span class="fa fa-search form-control-feedback"></span>
+          <input type="text" class="form-control"></input>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Search;
