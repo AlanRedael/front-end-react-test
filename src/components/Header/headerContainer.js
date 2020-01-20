34fc2437d0728248eaf4eb704fbb47e501996ab1
Alan G Sinimbú <ar.redael@gmail.com>
2020-01-20 15:54:05 -0300

    Code sanitazing

--- src/components/Header/headerContainer.js
+++ src/components/Header/headerContainer.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Search from '../Search/Search.js';
-//import './App.css';
 
 function headerContainer() {
   return (
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -6,9 +6,9 @@ function Search() {
     <div className="col-test-3">
       <div className="form-group flex-wrap: wrap">
         <label>Filter by name</label>
-        <div class="form-group has-search">
-          <span class="fa fa-search form-control-feedback"></span>
-          <input type="text" class="form-control"></input>
+        <div className="form-group has-search">
+          <span className="fa fa-search form-control-feedback"></span>
+          <input type="text" className="form-control"></input>
         </div>
       </div>
     </div>
